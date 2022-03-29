f1c744633cd0dd347c8da67d3a7d195d7fd3e7d9
lunapiresfernandescefet <alyssapiresfernandes@gmail.com>
2022-03-29 18:56:40 -0300

    Update PlayerAttributeField.tsx
    updated PlayerAttributeField UX

--- src/components/Player/Attribute/PlayerAttributeField.tsx
+++ src/components/Player/Attribute/PlayerAttributeField.tsx
@@ -48,6 +48,10 @@ export default function PlayerAttributeField(props: PlayerAttributeFieldProps) {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [barRef]);
 
+    useEffect(() => {
+        if (timeout.current) clearTimeout(timeout.current);
+    }, [maxValue]);
+
     function updateValue(ev: React.MouseEvent, coeff: number) {
         if (ev.shiftKey) coeff *= 10;
 
@@ -62,31 +66,26 @@ export default function PlayerAttributeField(props: PlayerAttributeFieldProps) {
             api.post('/sheet/player/attribute', { id: attributeID, value: newVal }).catch(logError), 750);
     }
 
-    function updateMaxValue(ev: FormEvent<HTMLInputElement>) {
-        const aux = ev.currentTarget.value;
-        let newMaxValue = parseInt(aux);
+    function onNewMaxValue() {
+        let input = prompt('Digite o novo valor do atributo:', maxValue.toString());
 
-        if (aux.length === 0) newMaxValue = 0;
-        else if (isNaN(newMaxValue)) return;
+        if (input === null) return;
+
+        const newMaxValue = parseInt(input);
+
+        if (isNaN(newMaxValue) || maxValue === newMaxValue) return;
 
         setMaxValue(newMaxValue);
-    }
-
-    function maxValueBlur() {
-        if (maxValue === lastMaxValue) return;
-
-        setMaxValue(maxValue);
         let valueUpdated = false;
-        if (value > maxValue) {
-            setValue(maxValue);
+        if (value > newMaxValue) {
+            setValue(newMaxValue);
             valueUpdated = true;
-        };
+        }
 
-        api.post('/sheet/player/attribute', { id: attributeID, maxValue, value: valueUpdated ? maxValue : undefined })
-            .catch(err => {
-                logError(err);
-                setMaxValue(lastMaxValue);
-            });
+        api.post('/sheet/player/attribute', {
+            id: attributeID, maxValue: newMaxValue,
+            value: valueUpdated ? newMaxValue : undefined
+        }).catch(logError);
     }
 
     function diceClick() {
@@ -104,7 +103,7 @@ export default function PlayerAttributeField(props: PlayerAttributeFieldProps) {
             </Row>
             <Row>
                 <Col>
-                    <ProgressBar now={value} min={0} max={maxValue} ref={barRef} />
+                    <ProgressBar now={value} min={0} max={maxValue} ref={barRef} className='clickable' onClick={onNewMaxValue} />
                 </Col>
                 {props.playerAttribute.Attribute.rollable &&
                     <Col xs='auto' className='align-self-center'>
@@ -116,21 +115,14 @@ export default function PlayerAttributeField(props: PlayerAttributeFieldProps) {
                 <Col xs lg={3}>
                     <Button variant='secondary' className='w-100' onClick={ev => updateValue(ev, -1)}>-</Button>
                 </Col>
-                <Col xs lg={2} className='text-center'>
-                    <label className='h5' htmlFor={`attribute${attributeID}`}>{`${value}/${maxValue}`}</label>
+                <Col xs lg={2} className='text-center align-self-center h5 m-0'>
+                    {`${value}/${maxValue}`}
                 </Col>
                 <Col xs lg={3}>
                     <Button variant='secondary' className='w-100' onClick={ev => updateValue(ev, 1)}>+</Button>
                 </Col>
             </Row>
-            <Row>
-                <Col xs={{ span: 4, offset: 4 }} lg={{ span: 2, offset: 5 }} className='h5' >
-                    <BottomTextInput maxLength={3} autoComplete='off' value={maxValue}
-                        onChange={updateMaxValue} id={`attribute${attributeID}`}
-                        className='text-center w-100' onBlur={maxValueBlur} />
-                </Col>
-            </Row>
-            <Row className='mb-3'>
+            <Row className='mt-2 mb-3'>
                 <Col>
                     {props.playerStatus.map(stat =>
                         <PlayerAttributeStatusField key={stat.AttributeStatus.id}
