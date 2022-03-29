290dcb1addf96a85d4245abf20f14eed0c75ceae
lunapiresfernandescefet <alyssapiresfernandes@gmail.com>
2022-03-29 17:43:47 -0300

    prisma config model bug fix

--- src/prisma/migrations/20220329204304_init_migration/migration.sql
+++ src/prisma/migrations/20220329204304_init_migration/migration.sql
@@ -253,10 +253,12 @@ CREATE TABLE `PlayerAvatar` (
 
 -- CreateTable
 CREATE TABLE `Config` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `value` JSON NOT NULL,
 
-    PRIMARY KEY (`name`)
+    UNIQUE INDEX `Config_name_key`(`name`),
+    PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
