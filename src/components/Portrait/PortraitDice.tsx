4233b312d6eee3bde206fe296df71300c55e35fc
Alyssa Pires Fernandes <alyssapiresfernandes@gmail.com>
2022-04-16 22:49:08 -0300

    Update PortraitDice.tsx
    bug fix

--- src/components/Portrait/PortraitDice.tsx
+++ src/components/Portrait/PortraitDice.tsx
@@ -23,68 +23,69 @@ export default function PortraitDice(props: {
 
 	const diceVideo = useRef<HTMLVideoElement>(null);
 
-	function showDiceRoll() {
-		if (showDiceRef.current) return;
-		showDiceRef.current = true;
-		if (diceVideo.current) {
-			props.onShowDice();
-			diceVideo.current.currentTime = 0;
-			diceVideo.current.play();
-		}
-	}
-
-	async function showNextResult(result: DiceResult) {
-		showDiceRoll();
-		await sleep(1000);
-		diceData.current = undefined;
-		onDiceResult(result);
-	}
-
-	async function onDiceResult(result: DiceResult) {
-		if (diceData.current) return diceQueue.current.push(result);
-		if (!showDiceRef.current) return showNextResult(result);
-
-		diceData.current = result;
-
-		lastDiceResult.current = result.roll;
-		setDiceResult(result.roll);
-
-		if (result.resultType) {
-			lastDiceDescription.current = result.resultType.description;
-			await sleep(750);
-			setDiceDescription(result.resultType.description);
-		}
-		await sleep(1500);
-
-		setDiceResult(null);
-		setDiceDescription(null);
-
-		await sleep(250);
-		props.onHideDice();
-		await sleep(600);
-		showDiceRef.current = false;
-		const next = diceQueue.current.shift();
-		if (next) showNextResult(next);
-	}
-
 	useEffect(() => {
 		const socket = props.socket;
 
 		if (!socket) return;
 
+		function showDiceRoll() {
+			if (showDiceRef.current) return;
+			showDiceRef.current = true;
+			if (diceVideo.current) {
+				props.onShowDice();
+				diceVideo.current.currentTime = 0;
+				diceVideo.current.play();
+			}
+		}
+
+		async function showNextResult(result: DiceResult) {
+			showDiceRoll();
+			await sleep(1000);
+			diceData.current = undefined;
+			onDiceResult(result);
+		}
+
+		async function onDiceResult(result: DiceResult) {
+			if (diceData.current) return diceQueue.current.push(result);
+			if (!showDiceRef.current) return showNextResult(result);
+
+			diceData.current = result;
+
+			lastDiceResult.current = result.roll;
+			setDiceResult(result.roll);
+
+			if (result.resultType) {
+				lastDiceDescription.current = result.resultType.description;
+				await sleep(750);
+				setDiceDescription(result.resultType.description);
+			}
+			await sleep(1500);
+
+			setDiceResult(null);
+			setDiceDescription(null);
+
+			await sleep(250);
+			props.onHideDice();
+			await sleep(600);
+			showDiceRef.current = false;
+
+			const next = diceQueue.current.shift();
+			if (next) showNextResult(next);
+			else diceData.current = undefined;
+		}
+
 		socket.on('diceRoll', showDiceRoll);
 		socket.on('diceResult', (playerId, results) => {
 			if (playerId !== props.playerId) return;
-
 			if (results.length === 1) onDiceResult(results[0]);
 			else if (results.length > 1) {
-				const rolls = results.map(result => result.roll);
+				const rolls = results.map((result) => result.roll);
 				onDiceResult({
 					roll: rolls.reduce((prev, cur) => prev + cur, 0),
 					resultType: {
 						description: rolls.join(' + '),
-						isSuccess: true
-					}
+						isSuccess: true,
+					},
 				});
 			}
 		});
