23c8e00fc80706071ca6ea322f428872c6add83e
Alyssa Pires Fernandes <alyssapiresfernandes@gmail.com>
2022-04-21 21:41:39 -0300

    added default heads and optional heads per page

--- src/components/ApplicationHead.tsx
+++ src/components/ApplicationHead.tsx
@@ -2,15 +2,13 @@ import Head from 'next/head';
 
 type ApplicationHeadProps = {
     title?: string,
-    children?: React.ReactElement
+    children?: React.ReactChild
 };
 
 export default function ApplicationHead({ title, children }: ApplicationHeadProps) {
     return (
         <Head>
             <title>{`${title || ''} - Open RPG`}</title>
-            <meta name="description" content='Powered by Open RPG. Para saber mais: https://github.com/alyssapiresfernandescefet/openrpg' />
-            <link rel="icon" href="/favicon.ico" />
             {children}
         </Head>
     );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/globals.scss';
 import { AppProps } from 'next/app';
 import SSRProvider from 'react-bootstrap/SSRProvider';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <SSRProvider>
-      <Component {...pageProps} />
-    </SSRProvider>
-  );
+	return (
+		<SSRProvider>
+			<Head>
+				<meta
+					name='description'
+					content='Site criado pelo aplicativo Open RPG. Para saber mais: https://github.com/alyssapiresfernandescefet/openrpg'
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</SSRProvider>
+	);
 }
