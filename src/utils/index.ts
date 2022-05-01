186a04e84679b36fca7081c24c3015e82e9b7026
Alyssa Pires Fernandes <alyssapiresfernandes@gmail.com>
2022-05-01 17:39:46 -0300

    global style code refactoring

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,9 +8,7 @@ export function sleep(ms: number): Promise<void> {
 	return new Promise((res) => setTimeout(res, ms));
 }
 
-type GetSSRResult<TProps> =
-	//
-	{ props: TProps } | { redirect: any } | { notFound: true };
+type GetSSRResult<TProps> = { props: TProps } | { redirect: any } | { notFound: true };
 
 type GetSSRFn<TProps extends any> = (args: any) => Promise<GetSSRResult<TProps>>;
 
